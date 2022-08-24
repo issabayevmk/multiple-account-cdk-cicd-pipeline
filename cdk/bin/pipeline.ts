9c246fe9ab186d6ac3663872f4253851c569b33c
Mustafa Issabayev <mustafa.issabayev@bitstamp.net>
2022-08-24 08:19:33 +0200

    s3

--- cdk/bin/pipeline.ts
+++ cdk/bin/pipeline.ts
@@ -5,9 +5,11 @@ import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
 const app = new App();
 
 new CdkPipelineStack(app, 'CdkPipelineStack', {
-  //env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  account: '118379544242',
-  region: 'eu-central-1'
+  env: { 
+    //account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION
+    account: '118379544242',
+    region: 'eu-central-1'
+  },
 });
 
 app.synth();
