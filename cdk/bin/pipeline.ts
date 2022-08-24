7980f73dea53101637b08d8ce255ab18fe96f6f6
issabayevmk <48990804+issabayevmk@users.noreply.github.com>
2022-08-24 06:46:10 +0200

    Update pipeline.ts

--- cdk/bin/pipeline.ts
+++ cdk/bin/pipeline.ts
@@ -5,7 +5,9 @@ import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
 const app = new App();
 
 new CdkPipelineStack(app, 'CdkPipelineStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  //env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  account: '118379544242',
+  region: 'eu-central-1'
 });
 
 app.synth();
