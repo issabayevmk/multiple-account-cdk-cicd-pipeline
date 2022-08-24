90619e77f9167eb33de7430cc9ccd1b4c9fc14ce
Mustafa Issabayev <mustafa.issabayev@bitstamp.net>
2022-08-24 08:34:06 +0200

    s3

--- cdk/lib/s3-stack.ts
+++ cdk/lib/s3-stack.ts
@@ -2,14 +2,15 @@ import { Stack, StackProps, Construct, CfnOutput } from "@aws-cdk/core";
 import { S3 } from "@aws-cdk/aws-s3";
 
 export interface S3StackProps extends StackProps {
-    env: {
+    /*env: {
         account: '118379544242',
         region: 'eu-central-1'
-      },
+      },*/
+      name: string
 }
 
 export class S3Stack extends Stack {
-    constructor(scope: Construct, id: string, props: S3StackProps) {
+    constructor(scope: Construct, id: string, props?: S3StackProps) {
         super(scope, id, props);
 
         const bucket = new S3.Bucket(this, 'SecurePipelineBucket', {
